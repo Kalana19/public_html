19ffc3e1142433543eec560717ae726c71092f5e
Kalana Gayan Jayasinghe <s4142933@csitprdap02.int.its.rmit.edu.au>
2025-10-05 15:49:48 +1100

    final

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 function getPDO() {
-    $host = 'saturn';
-    $dbname = 'wp_group_db';  
-    $username = 'root';  
-    $password = '';  
+    $host = 'talsprddb02.int.its.rmit.edu.au'; // from your email
+    $port = 3306; // from your email
+    $dbname = 'COSC3046_2550_G13'; // your database name
+    $username = 'COSC3046_2550_G13'; // your username
+    $password = 'hUKqh6eEU2Gc'; // your password
 
     try {
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
